f03b41702533ba3158bbe6cfd37e0a43cf2a7eb2
FSkyCode <juanfelipesierracastillo@gmail.com>
2025-10-29 23:44:30 -0500

    Actualizar index.js

--- expressions/index.js
+++ expressions/index.js
@@ -1,1 +1,15 @@
-j
+// expressions/index.js
+import fs from "fs"
+import path from "path"
+
+const expressions = {}
+const folder = path.resolve("./expressions")
+
+for (const file of fs.readdirSync(folder)) {
+  if (file.endsWith(".js") && file !== "index.js") {
+    const exp = await import(`./${file}`)
+    expressions[exp.default.name] = exp.default
+  }
+}
+
+export default expressions
