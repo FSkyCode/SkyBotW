9a89d2639fa23189d3538deab357715a6d7d30f9
FSkyCode <juanfelipesierracastillo@gmail.com>
2025-11-15 19:53:01 -0500

    Actualizar index.js

--- commands/index.js
+++ commands/index.js
@@ -2,9 +2,11 @@
 import establecerBot from "./establecerBot.js"
 import busquedaEspecifica from "./busquedaEspecifica.js"
 import video from "./buscarVideo.js"
+import apiVideo from "./apiVideo.js"
 
 export default {
   "!establecerbot": establecerBot,
   "!busquedaespecifica": busquedaEspecifica,
   "!video": video,
+  "!apivideo": apiVideo,
 }
